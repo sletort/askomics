7b9aa1900e834446a743cfe762f26060bbe1bfe8
Sébastien Letort <sletort@irisa.fr>
2018-11-27 08:38:55 +0000

    Abstraction has been generalized to whatever, not limited to file anymore. ExternalEndpoint.py is a class to deal with external endpoint integration, its place is just a suggestion (in which case source_file is not a good name).

--- askomics/libaskomics/integration/AbstractedEntity.py
+++ askomics/libaskomics/integration/AbstractedEntity.py
@@ -7,35 +7,47 @@ import json
 from askomics.libaskomics.ParamManager import ParamManager
 from askomics.libaskomics.utils import pformat_generic_object
 
-class AbstractedEntity(object):
+class AbstractedEntity__( object ):
     """
     An AbstractedEntity represents the classes of the database.
     It is defined by an uri and a label.
     """
-
-    def __init__(self, identifier,prefix):
-        self.uri = ParamManager.encode_to_rdf_uri(identifier,prefix)
-        self.label = identifier
+    def __init__( self, uri, label, startpoint=False ):
+        self.__uri   = uri
+        self.__label = label
+        self.__startpoint = startpoint
 
         self.log = logging.getLogger(__name__)
 
-    def get_uri(self):
-        return self.uri
+    @property
+    def _uri( self ):
+        return self.__uri
+    @property
+    def _label( self ):
+        return self.__label
 
     def get_turtle(self):
         """
         return the turtle code describing an AbstractedEntity
         for the abstraction file generation.
         """
-        turtle = self.get_uri() + " rdf:type owl:Class ;\n"
+        turtle = self._uri + "\n"
 
-        turtle += (len(self.get_uri()) + 1) * " " + "askomics:entity \"true\"^^xsd:boolean ;\n"
-        turtle += (len(self.get_uri()) + 1) * " " + "rdfs:label " + json.dumps(self.label) + "^^xsd:string .\n\n"
-        turtle += '\n'
-        turtle += 'rdfs:label rdf:type owl:DatatypeProperty .\n'
-        turtle += 'rdfs:label askomics:attribute "true"^^xsd:boolean .\n'
-        turtle += 'rdfs:label askomics:attributeOrder "1"^^xsd:decimal .\n'
-        turtle += 'rdfs:label rdfs:label "label" .\n'
-        turtle += 'rdfs:label rdfs:domain '+self.get_uri()+' .\n'
-        turtle += 'rdfs:label rdfs:range xsd:string .\n'
+        turtle += (len(self._uri) + 1) * " " + "askomics:entity \"true\"^^xsd:boolean ;\n"
+        turtle += (len(self._uri) + 1) * " " + "rdfs:label " + json.dumps(self._label) + "^^xsd:string ;\n"
+        if self.__startpoint:
+            turtle += (len(self._uri) + 1) * " " + "askomics:startPoint \"true\"^^xsd:boolean ;\n"
+        turtle += '.\n\n'
+
         return turtle
+
+class AbstractedEntity( AbstractedEntity__ ):
+    """
+    An AbstractedEntity represents the classes of the database.
+    It is defined by an uri and a label.
+    """
+
+    def __init__(self, identifier,prefix):
+        uri = ParamManager.encode_to_rdf_uri(identifier,prefix)
+        label = identifier
+        __AbstractedEntity.__init__( uri, label )
